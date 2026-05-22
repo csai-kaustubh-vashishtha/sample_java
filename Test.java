0d57891410476d6eaddd53d3f0a2d3721fdabc23
csai-kaustubh-vashishtha <csai.kaustubh.vashishtha@gmail.com>
2026-05-22 12:00:12 +0530

    updated5

--- Test.java
+++ Test.java
@@ -2,10 +2,10 @@ public class Test {
 
   static void add(){
 
-    int a=48;
+    int a=40;
     int b=65;
     int result = a-b;
-    System.out.println("The sum is " +result);
+    System.out.println("The diff is " +result);
     
   }
   public static void main(String[] args)
